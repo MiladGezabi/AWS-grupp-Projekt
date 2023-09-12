856ced8a863a3fc4ea4972f3d4672bb104d9f02e
MiladGezabi <milad.gezabi@gmail.com>
2023-09-12 11:16:39 +0200

    added a post request in upload route

--- src/Routs/Upload.jsx
+++ src/Routs/Upload.jsx
@@ -1,19 +1,99 @@
+import { useState } from "react";
 
 
 function Upload() {
+  const [imgFile, setImgFile] = useState(null);
+  const [text, setText] = useState("");
+  const [imgBuffer, setImgBuffer] = useState(null);
 
-  return(
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setImgFile(file);
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        const buffer = event.target.result;
+        setImgBuffer(buffer);
+      };
+      reader.readAsArrayBuffer(file);
+    }
+  };
+
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, "0"); 
+    const day = String(currentDate.getDate()).padStart(2, "0");
+
+    const formattedDate = `${year}-${month}-${day}`;
+
+
+    const formData = new FormData();
+    if (imgBuffer) {
+      formData.append("image", new Blob([imgBuffer], { type: imgFile.type }));
+    }
+    formData.append("text", text);
+    formData.append("date", formattedDate);
+
+    try {
+      const response = await fetch("Url", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        console.log("Datan är uppladad");
+      } else {
+        console.error("Error laddar upp data");
+      }
+    } catch (error) {
+      console.error("oops gick inte:", error);
+    }
+  };
+
+  const handleHover=() => {
+    const obs=document.querySelector(".obs")
+    obs.classList.toggle("visible")
+  }
+
+  return (
     <section className="upload-section">
       <h2>Upload</h2>
-      <form action="">
-        <label htmlFor="img">Select image*</label>
-        <input type="file" id="img" name="img" accept="image/*" />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="img"
+        onMouseEnter={handleHover}
+        onMouseLeave={handleHover}
+        >Select image
+        <span className="span">* 
+          <p className="obs">only image files are accepted
+          </p> 
+          </span>
+        </label>
+        <input
+          type="file"
+          id="img"
+          name="img"
+          accept="image/*"
+          onChange={handleFileChange}
+        />
         <label htmlFor="text">Describe image*</label>
-        <textarea name="text" id="text" cols="30" rows="10" />
-        <button>Upload</button>
+        <textarea
+          name="text"
+          id="text"
+          cols="30"
+          rows="10"
+          onChange={handleTextChange}
+        />
+        <button type="submit">Upload</button>
       </form>
     </section>
-  )
+  );
 }
 
-export default Upload
+export default Upload;
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from "react-router-dom"
 import { router } from './RoutConfig.jsx'
+import { RecoilRoot } from 'recoil'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <RecoilRoot>
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
+  </RecoilRoot>
 )
