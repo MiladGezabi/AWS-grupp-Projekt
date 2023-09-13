2fd81a17b3351bfa5d16594d5fbd3b688b8a1575
MiladGezabi <milad.gezabi@gmail.com>
2023-09-13 14:43:51 +0200

    added a get request on browse route

--- src/Routs/Browse.jsx
+++ src/Routs/Browse.jsx
@@ -1,6 +1,23 @@
-
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function Browse() {
+  const [data, setData] =useState([])
+
+  useEffect(() => {
+    
+    const url = 'https://1k5gjm0ree.execute-api.eu-north-1.amazonaws.com/test/s3fetchmanager';
+
+    
+    axios.get(url)
+      .then((response) => {
+        setData(response.data); 
+        console.log(data)
+      })
+      .catch((error) => {
+        console.error('There was a problem with the Axios request:', error);
+      });
+  }, []);
 
   return(
     <section className="browse-section">
