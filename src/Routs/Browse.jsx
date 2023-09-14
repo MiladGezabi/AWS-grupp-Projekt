d926afb256afbdc7afc5868a116084076c4a2747
MiladGezabi <milad.gezabi@gmail.com>
2023-09-14 14:51:18 +0200

    m

--- src/Routs/Browse.jsx
+++ src/Routs/Browse.jsx
@@ -5,29 +5,9 @@ function Browse() {
   const [data, setData] = useState([]);
   const [isData, setIsData] = useState(false);
   const [sort, setSort] = useState("Latest");
+  const [sortedArray, setSortedArray] = useState([]); // Skapa en tom array som börjar med []
+  const [showSortedData, setShowSortedData] = useState(false); // Visa sorterad data eller ej
 
-  function sortByLatestUploadDate() {
-    const sortedData = [...data].sort(
-      (a, b) => {
-        const dateA = parseInt(a.Metadata.uploaddate.replace("-", ""), 10)
-        const dateB = parseInt(a.Metadata.uploaddate.replace("-", ""), 10)
-        
-        return dateA - dateB
-    
-      });
-      console.log(sortedData);
-  }
-
-
-  function handleSort() {
-    if (sort === "Latest") {
-      const sortedItemsLatestFirst = [...data].sort((a, b) => new Date(b.date) - new Date(a.date));
-      console.log(sortedItemsLatestFirst)
-    } else if (sort === "Oldest") {
-      const sortedItemsEarliestFirst = [...data].sort((a, b) => new Date(a.date) - new Date(b.date));
-      console.log(sortedItemsEarliestFirst)
-    }
-  }
   useEffect(() => {
     const url =
       "https://1k5gjm0ree.execute-api.eu-north-1.amazonaws.com/test/s3fetch";
@@ -45,31 +25,53 @@ function Browse() {
   useEffect(() => {
     if (data.length > 0) {
       console.log(data.length);
-      // data.forEach(
-      //   (item) => (
-      //     console.log(item),
-      //     console.log(
-      //       "https://spicypants-s3bucket.s3.eu-north-1.amazonaws.com/" +
-      //         item.Key
-      //     )
-      //   )
-      // );
       setIsData(true);
     }
   }, [data]);
+
+  function sortByOldestUploadDate() {
+    // Sortera datan
+    const sorted = [...data].sort((a, b) =>
+      Number(a.Metadata.uploaddate.replaceAll("-", "")) -
+      Number(b.Metadata.uploaddate.replaceAll("-", ""))
+    );
+
+    // Uppdatera sortedArray
+    setSortedArray(sorted);
+    setShowSortedData(true); // Visa sorterad data när den är klar att användas
+  }
+
+  function sortByLatestUploadDate() {
+    // Sortera datan
+    const sorted = [...data].sort((a, b) =>
+      Number(b.Metadata.uploaddate.replaceAll("-", "")) -
+      Number(a.Metadata.uploaddate.replaceAll("-", ""))
+    );
+
+    // Uppdatera sortedArray
+    setSortedArray(sorted);
+    setShowSortedData(true); // Visa sorterad data när den är klar att användas
+  }
 
   return (
     <section className="browse-section">
       <div className="search-filter">
         <h2>Browse</h2>
-        <button className="sorting" onClick={() => so()}>
-          {sort}
+        <button className="sorting" onClick={
+          () => (sortByOldestUploadDate())
+          }>
+          {'Äldst först'}
+        </button>
+        <button className="sorting" onClick={
+          () => (sortByLatestUploadDate())
+          }>
+          {'Senaste först'}
         </button>
       </div>
 
       <div className="picture-container">
         {isData &&
-          data.map((img, index) => (
+          (showSortedData ? sortedArray : data).map((img, index) => (
             <figure key={index}>
               <p className="date">{img.Metadata.uploaddate}</p>
               <img
@@ -78,7 +80,6 @@ function Browse() {
                   img.Key
                 }
               />
-
               <p className="description">{img.Metadata.description}</p>
             </figure>
           ))}
